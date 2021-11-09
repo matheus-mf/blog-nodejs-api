d102e0eeff1c8c9c7f385c917f81a7bd1a49a5aa
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-11-09 10:51:19 -0300

    feat(post): creating cases that cannot be updated

--- src/modules/posts/useCases/updatePost/UpdatePostUseCase.spec.ts
+++ src/modules/posts/useCases/updatePost/UpdatePostUseCase.spec.ts
@@ -1,6 +1,7 @@
 import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
 import { CategoriesRepositoryInMemory } from "@modules/posts/repositories/in-memory/CategoriesRepositoryInMemory";
 import { PostsRepositoryInMemory } from "@modules/posts/repositories/in-memory/PotsRepositoryInMemory";
+import AppError from "@shared/errors/AppError";
 
 import { UpdatePostUseCase } from "./UpdatePostUseCase";
 
@@ -15,7 +16,11 @@ describe("Update Post", () => {
     usersRepositoryInMemory = new UsersRepositoryInMemory();
     categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
 
-    updatePostUseCase = new UpdatePostUseCase(postsRepositoryInMemory);
+    updatePostUseCase = new UpdatePostUseCase(
+      postsRepositoryInMemory,
+      usersRepositoryInMemory,
+      categoriesRepositoryInMemory
+    );
   });
 
   it("should be able update a post", async () => {
@@ -52,5 +57,113 @@ describe("Update Post", () => {
     });
 
     expect(posts.title).toEqual("update title");
+  });
+
+  it("should not be able update a post when user does not exist", async () => {
+    const user = {
+      name: "string",
+      email: "user@example.com",
+    };
+
+    const autor = await usersRepositoryInMemory.create(user);
+
+    const category = {
+      name: "string",
+    };
+
+    await categoriesRepositoryInMemory.create(category);
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    const postCreated = await postsRepositoryInMemory.create({
+      user_id: autor.id,
+      category_id: categoryCreated.id,
+      description: "description",
+      title: "title",
+    });
+
+    await expect(
+      updatePostUseCase.execute({
+        id: postCreated.id,
+        title: "update title",
+        description: postCreated.description,
+        category_id: postCreated.category_id,
+        user_id: "user-not-exist",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able update a post when category does not exist", async () => {
+    const user = {
+      name: "string",
+      email: "user@example.com",
+    };
+
+    const autor = await usersRepositoryInMemory.create(user);
+
+    const category = {
+      name: "string",
+    };
+
+    await categoriesRepositoryInMemory.create(category);
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    const postCreated = await postsRepositoryInMemory.create({
+      user_id: autor.id,
+      category_id: categoryCreated.id,
+      description: "description",
+      title: "title",
+    });
+
+    await expect(
+      updatePostUseCase.execute({
+        id: postCreated.id,
+        title: "update title",
+        description: postCreated.description,
+        category_id: "category-not-exist",
+        user_id: postCreated.user_id,
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to update a post when the post is not found", async () => {
+    const user = {
+      name: "string",
+      email: "user@example.com",
+    };
+
+    const autor = await usersRepositoryInMemory.create(user);
+
+    const category = {
+      name: "string",
+    };
+
+    await categoriesRepositoryInMemory.create(category);
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    const postCreated = await postsRepositoryInMemory.create({
+      user_id: autor.id,
+      category_id: categoryCreated.id,
+      description: "description",
+      title: "title",
+    });
+
+    await expect(
+      updatePostUseCase.execute({
+        id: "post-not-found",
+        title: "update title",
+        description: postCreated.description,
+        category_id: postCreated.category_id,
+        user_id: postCreated.user_id,
+      })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
--- src/modules/posts/useCases/updatePost/UpdatePostUseCase.ts
+++ src/modules/posts/useCases/updatePost/UpdatePostUseCase.ts
@@ -1,7 +1,10 @@
 import { inject, injectable } from "tsyringe";
 
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { Post } from "@modules/posts/infra/typeorm/entities/Post";
+import { ICategoriesRepository } from "@modules/posts/repositories/ICategoriesRepository";
 import { IPostsRepository } from "@modules/posts/repositories/IPostsRepository";
+import AppError from "@shared/errors/AppError";
 
 interface IRequest {
   id: string;
@@ -15,7 +18,13 @@ interface IRequest {
 class UpdatePostUseCase {
   constructor(
     @inject("PostsRepository")
-    private postsRepository: IPostsRepository
+    private postsRepository: IPostsRepository,
+
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
   ) {}
 
   public async execute({
@@ -25,7 +34,25 @@ class UpdatePostUseCase {
     category_id,
     user_id,
   }: IRequest): Promise<Post> {
+    const userDoesNotExist = await this.usersRepository.findById(user_id);
+
+    if (!userDoesNotExist) {
+      throw new AppError("User does not exist!");
+    }
+
+    const categoryDoesNotExist = await this.categoriesRepository.findById(
+      category_id
+    );
+
+    if (!categoryDoesNotExist) {
+      throw new AppError("Category does not exist!");
+    }
+
     const post = await this.postsRepository.findById(id);
+
+    if (!post) {
+      throw new AppError("Post does not exist!", 404);
+    }
 
     Object.assign(post, {
       description,
