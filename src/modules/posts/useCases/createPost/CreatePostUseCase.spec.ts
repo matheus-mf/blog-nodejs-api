b6e1f92ec1a1f7e93a47acdfd6ded178bd5022d4
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-11-09 00:31:54 -0300

    feat(post): cannot create user post does not exist

--- src/modules/posts/useCases/createPost/CreatePostUseCase.spec.ts
+++ src/modules/posts/useCases/createPost/CreatePostUseCase.spec.ts
@@ -1,6 +1,7 @@
 import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
 import { CategoriesRepositoryInMemory } from "@modules/posts/repositories/in-memory/CategoriesRepositoryInMemory";
 import { PostsRepositoryInMemory } from "@modules/posts/repositories/in-memory/PotsRepositoryInMemory";
+import AppError from "@shared/errors/AppError";
 
 import { CreatePostUseCase } from "./CreatePostUseCase";
 
@@ -11,11 +12,14 @@ let createPostUseCase: CreatePostUseCase;
 
 describe("Create Post", () => {
   beforeEach(() => {
-    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
-    usersRepositoryInMemory = new UsersRepositoryInMemory();
-
     postsRepositoryInMemory = new PostsRepositoryInMemory();
-    createPostUseCase = new CreatePostUseCase(postsRepositoryInMemory);
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+
+    createPostUseCase = new CreatePostUseCase(
+      postsRepositoryInMemory,
+      usersRepositoryInMemory
+    );
   });
 
   it("should be able to create a new post", async () => {
@@ -44,5 +48,26 @@ describe("Create Post", () => {
     });
 
     expect(postCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new post when user does not exist", async () => {
+    const category = {
+      name: "Category Test",
+    };
+
+    await categoriesRepositoryInMemory.create(category);
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    await expect(
+      createPostUseCase.execute({
+        user_id: "user-not-exist",
+        category_id: categoryCreated.id,
+        description: "description",
+        title: "title",
+      })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
--- src/modules/posts/useCases/createPost/CreatePostUseCase.ts
+++ src/modules/posts/useCases/createPost/CreatePostUseCase.ts
@@ -1,7 +1,9 @@
 import { inject, injectable } from "tsyringe";
 
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { Post } from "@modules/posts/infra/typeorm/entities/Post";
 import { IPostsRepository } from "@modules/posts/repositories/IPostsRepository";
+import AppError from "@shared/errors/AppError";
 
 interface IRequest {
   user_id: string;
@@ -14,7 +16,10 @@ interface IRequest {
 class CreatePostUseCase {
   constructor(
     @inject("PostsRepository")
-    private postsRepository: IPostsRepository
+    private postsRepository: IPostsRepository,
+
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
   ) {}
 
   public async execute({
@@ -23,6 +28,12 @@ class CreatePostUseCase {
     description,
     title,
   }: IRequest): Promise<Post> {
+    const userDoesNotExist = await this.usersRepository.findById(user_id);
+
+    if (!userDoesNotExist) {
+      throw new AppError("User does not exist!");
+    }
+
     return this.postsRepository.create({
       user_id,
       category_id,
