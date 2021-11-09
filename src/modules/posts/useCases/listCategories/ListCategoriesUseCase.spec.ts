88361a412785c76095fd0ebe92a61b7bdb236f42
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-11-09 12:05:45 -0300

    feat(post): list all categories

---
+++ src/modules/posts/useCases/listCategories/ListCategoriesUseCase.spec.ts
@@ -0,0 +1,31 @@
+import { CategoriesRepositoryInMemory } from "@modules/posts/repositories/in-memory/CategoriesRepositoryInMemory";
+
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+let listCategoriesUseCase: ListCategoriesUseCase;
+
+describe("List Categories", () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    listCategoriesUseCase = new ListCategoriesUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it("should be able to list all categories", async () => {
+    const category1 = {
+      name: "string 1",
+    };
+    const category2 = {
+      name: "string 1",
+    };
+
+    await categoriesRepositoryInMemory.create(category1);
+    await categoriesRepositoryInMemory.create(category2);
+
+    const posts = await listCategoriesUseCase.execute();
+
+    expect(posts.length).toEqual(2);
+  });
+});
--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateCategoryController } from "@modules/posts/useCases/createCategory/CreateCategoryController";
+import { ListCategoriesController } from "@modules/posts/useCases/listCategories/ListCategoriesController";
 
 const categoriesRoutes = Router();
 
 const createCategoryController = new CreateCategoryController();
+const listCategoriesController = new ListCategoriesController();
 
 categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.get("/", listCategoriesController.handle);
 
 export default categoriesRoutes;
