723e2ad3cac84bde747c7c3686f727d382d4a466
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-11-08 23:46:41 -0300

    feat(post): migration to create posts

---
+++ src/shared/infra/typeorm/migrations/1636425599067-CreatePosts.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreatePosts1636425599067 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "posts",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+            isNullable: true,
+          },
+          {
+            name: "category_id",
+            type: "uuid",
+            isNullable: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FK_UserPost",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+          {
+            name: "FK_CategoryPost",
+            referencedTableName: "categories",
+            referencedColumnNames: ["id"],
+            columnNames: ["category_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("posts");
+  }
+}
